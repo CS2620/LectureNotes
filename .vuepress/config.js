a4a2aabaed9f73b547fa76586522c48fb711cf82
B Ricks <bricks@unomaha.edu>
2019-08-26 09:52:26 -0500

    Add Day 1

---
+++ .vuepress/config.js
@@ -0,0 +1,17 @@
+module.exports={
+  title:'2D Graphics: Image Processing',
+  description: "Lecture Notes for CSCI 2620, 2D Graphics: Image Processing, a computer science course at the University of Nebraska at Omaha.",
+  themeConfig: {
+    nav: [
+      { text: 'Home', link: '/' },
+      
+    ],
+    sidebar: [
+      '/',
+      '/Day1',
+      
+      
+    ],
+    
+  }
+}
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# LectureNotes
+# 2D Graphics
+
+This is the homepage for  CSCI 2620, 2D Graphics: Image Processing, a computer science course at the University of Nebraska at Omaha.
+
+[Day 1 Lecture Notes](Day1.md)
